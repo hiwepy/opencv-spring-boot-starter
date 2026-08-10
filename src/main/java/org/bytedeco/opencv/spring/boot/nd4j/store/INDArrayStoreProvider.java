d765aa30371b546341350a9badc2111799aa4b07
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:47:02 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/bytedeco/opencv/spring/boot/nd4j/store/INDArrayStoreProvider.java
+++ src/main/java/org/bytedeco/opencv/spring/boot/nd4j/store/INDArrayStoreProvider.java
@@ -20,7 +20,7 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 /**
  * TODO
  * 
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public interface INDArrayStoreProvider {
 
