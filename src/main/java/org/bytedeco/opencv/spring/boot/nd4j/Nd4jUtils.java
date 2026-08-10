3035f20c2926f0096db726174f0997ef05bbdc91
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:47:22 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/bytedeco/opencv/spring/boot/nd4j/Nd4jUtils.java
+++ src/main/java/org/bytedeco/opencv/spring/boot/nd4j/Nd4jUtils.java
@@ -21,7 +21,7 @@ import org.nd4j.linalg.indexing.NDArrayIndex;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class Nd4jUtils {
 	
